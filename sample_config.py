f5464612aab20027a767a55aa79d6e66599ff71d
Cute Inspire <63757267+Randi356@users.noreply.github.com>
2022-03-08 07:39:25 +0700

    Create sample_config.py

---
+++ sample_config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Copyright @rencprx
+# FROM https://github.com/Randi356/Powerfull-BotStatus
+import os
+
+class Config(object):
+
+      API_ID = ""
+      API_HASH = ""
+      BOT_LIST = "VegetaApiBot VegetaSessionBot VegetaJsonBot MagicStylish_Bot ImageEditorPlus_Bot editor_premium_bot"
+      SESSION_NAME = ""
+      CHANNEL_OR_GROUP_ID = "" 
+      MESSAGE_ID = "" 
+      BOT_ADMIN_IDS = ""
+      TIME_ZONE = "Asia/Jakarta" 
+      
